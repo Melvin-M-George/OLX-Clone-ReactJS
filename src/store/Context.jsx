59075fbecd29e1c282e8c143e597a1f3bab5d4fc
Melvin-M-George <melvinmgeorge1234@gmail.com>
2025-03-04 15:27:30 +0530

    removed unnecessary folder

--- src/store/Context.jsx
+++ src/store/Context.jsx
@@ -4,7 +4,7 @@ export const FirebaseContext = createContext(null);
 export const AuthContext = createContext(null);
 
 export default function Context({children}){
-    const [user,setUser] = useState()
+    const [user,setUser] = useState(null)
 
     return(
         <AuthContext.Provider value={{user,setUser}}>
